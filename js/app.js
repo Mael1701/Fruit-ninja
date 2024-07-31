942d523f7559881484e3e825b78de6fd024cc981
Mael Micout <gh api micoutmael3@gmail.com>
2024-08-01 01:04:30 +0200

    re-build classes, and add game over windows

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 window.onload = () => {
   const scoreElement = document.getElementsByClassName('score')[0];
+  const gameOverFruitModal = document.getElementById('gameoverfruit');
+  const gameOverBombModal = document.getElementById('gameoverbomb');
+  const retryFruitButton = document.getElementById('retry-fruit');
+  const retryBombButton = document.getElementById('retry-bomb');
+  const scoreFruitElement = document.getElementById('score-fruit');
+  const scoreBombElement = document.getElementById('score-bomb');
+
+  let gameInterval;
+  let isGameOver = false;
+  const fruits = [];
+  const bombs = [];
 
   class Player {
     constructor() {
@@ -12,27 +23,29 @@ window.onload = () => {
     }
 
     resetScore() {
-      this.score = 0 ;
+      this.score = 0;
       scoreElement.innerHTML = this.score;
+    }
+
+    getScore() {
+      return this.score;
     }
   }
 
   const player = new Player();
 
-  class Fruit {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.img = document.getElementById('logo');
-      this.div = document.getElementById('game');
+  class GameObject {
+    constructor(imgSrc, containerId, speed, className) {
+      this.img = document.createElement('img');
+      this.img.src = imgSrc;
+      this.img.className = className;
+      this.div = document.getElementById(containerId);
+      this.speed = speed;
+      this.div.appendChild(this.img);
       this.init();
     }
 
     init() {
-      this.img.onclick = () => {
-        player.incrementScore();
-        this.resetPosition();
-      };
       this.resetPosition();
     }
 
@@ -42,52 +55,96 @@ window.onload = () => {
     }
 
     move() {
-      let currentBottom = parseInt(this.img.style.bottom, 10) || 0;
-      this.img.style.bottom = `${currentBottom - 4}px`;
+      if (!isGameOver) {
+        let currentBottom = parseInt(this.img.style.bottom, 10) || 0;
+        this.img.style.bottom = `${currentBottom - this.speed}px`;
 
-      if (currentBottom <= 0) {
-        this.resetPosition();
-        player.resetScore();
+        if (currentBottom <= 0) {
+          this.resetPosition();
+        }
       }
     }
   }
 
-  const fruit = new Fruit();
-
-  setInterval(() => fruit.move(), 30);
-
-  class Bomb {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.img1 = document.getElementById('logo1');
-      this.div = document.getElementById('game');
-      this.init();
-    }
-
-    init() {
-      this.img1.onclick = () => {
-        player.resetScore();
-        this.resetPosition();
+  class Fruit extends GameObject {
+    constructor(imgSrc, containerId, speed) {
+      super(imgSrc, containerId, speed, 'fruit');
+      this.img.onclick = () => {
+        if (!isGameOver) {
+          player.incrementScore();
+          this.resetPosition();
+        }
       };
-      this.resetPosition();
-    }
-
-    resetPosition() {
-      this.img1.style.left = `${Math.floor(Math.random() * (this.div.clientWidth - this.img1.width))}px`;
-      this.img1.style.bottom = '570px';
     }
 
     move() {
-      let currentBottom = parseInt(this.img1.style.bottom, 10) || 0;
-      this.img1.style.bottom = `${currentBottom - 4}px`;
-
-      if (currentBottom <= 0) {
-        this.resetPosition();
+      super.move();
+      if (parseInt(this.img.style.bottom, 10) <= 0) {
+        if (!isGameOver) {
+          scoreFruitElement.innerHTML = player.getScore();
+          showModal(gameOverFruitModal);
+        }
       }
     }
   }
-  const bomb = new Bomb();
 
-  setInterval(() => bomb.move(), 30);
+  class Bomb extends GameObject {
+    constructor(imgSrc, containerId, speed) {
+      super(imgSrc, containerId, speed, 'bomb');
+      this.img.onclick = () => {
+        if (!isGameOver) {
+          scoreBombElement.innerHTML = player.getScore();
+          showModal(gameOverBombModal);
+        }
+      };
+    }
+
+    move() {
+      super.move();
+    }
+  }
+
+  function createGameObjects(count, type) {
+    const objects = [];
+    for (let i = 0; i < count; i++) {
+      const imgSrc = type === 'fruit' ? './assets/img/banane.jpg' : './assets/img/bomb.png';
+      const speed = type === 'fruit' ? 1 : 1.5;
+      const object = type === 'fruit' ? new Fruit(imgSrc, 'game', speed) : new Bomb(imgSrc, 'game', speed);
+      objects.push(object);
+    }
+    return objects;
+  }
+
+  function showModal(modal) {
+    isGameOver = true;
+    clearInterval(gameInterval);
+    modal.style.display = 'flex';
+  }
+
+  function resetGame() {
+    isGameOver = false;
+    player.resetScore();
+    fruits.forEach(fruit => fruit.resetPosition());
+    bombs.forEach(bomb => bomb.resetPosition());
+    gameInterval = setInterval(() => {
+      fruits.forEach(fruit => fruit.move());
+      bombs.forEach(bomb => bomb.move());
+    }, 30);
+  }
+
+  retryFruitButton.onclick = () => {
+    gameOverFruitModal.style.display = 'none';
+    resetGame();
+  };
+
+  retryBombButton.onclick = () => {
+    gameOverBombModal.style.display = 'none';
+    resetGame();
+  };
+
+  // Create multiple fruits and bombs
+  fruits.push(...createGameObjects(5, 'fruit'));
+  bombs.push(...createGameObjects(5, 'bomb'));
+
+  resetGame();
 };
